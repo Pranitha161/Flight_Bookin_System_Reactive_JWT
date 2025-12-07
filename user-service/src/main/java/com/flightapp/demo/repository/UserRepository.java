5d17ff21a3f922f738018e196e07ee397ffb0340
Pranitha <pranithareddy160805@gmail.com>
2025-12-07 16:08:44 +0530

    Modified repository

--- user-service/src/main/java/com/flightapp/demo/repository/UserRepository.java
+++ user-service/src/main/java/com/flightapp/demo/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.flightapp.demo.repository;
 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
 import com.flightapp.demo.entity.User;
 
 import reactor.core.publisher.Mono;
 
-public interface UserRepository {
+public interface UserRepository extends ReactiveMongoRepository<User, String>{
 
 	Mono<User> findByUsername(String username);
 
