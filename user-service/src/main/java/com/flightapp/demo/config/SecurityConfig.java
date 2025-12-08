e519a5fccec58a6096d2923ae529783573bca79b
Pranitha <pranithareddy160805@gmail.com>
2025-12-08 13:04:09 +0530

    Modified config and filter

--- user-service/src/main/java/com/flightapp/demo/config/SecurityConfig.java
+++ user-service/src/main/java/com/flightapp/demo/config/SecurityConfig.java
@@ -1,5 +1,35 @@
 package com.flightapp.demo.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+//	 private final JwtAuthFilter jwtAuthFilter;
+//
+//	    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+//	        this.jwtAuthFilter = jwtAuthFilter;
+//	    }
+//	@Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
+//				auth -> auth.requestMatchers("/auth/signup", "/auth/login").permitAll().anyRequest().authenticated())
+//				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//		return http.build();
+//	}
 }
+
--- user-service/src/main/java/com/flightapp/demo/util/JwtUtil.java
+++ user-service/src/main/java/com/flightapp/demo/util/JwtUtil.java
@@ -4,14 +4,10 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -31,31 +27,31 @@ public class JwtUtil {
 				.compact();
 	}
 
-	public boolean validateToken(String token) {
-		try {
-			Claims claims = Jwts.parserBuilder()
-					.setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8))).build()
-					.parseClaimsJws(token).getBody();
-
-			return claims.getExpiration() != null && claims.getExpiration().after(new Date());
-		} catch (Exception e) {
-			return false;
-		}
-	}
-
-	public String extractUsername(String token) {
-		return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8))).build()
-				.parseClaimsJws(token).getBody().getSubject();
-	}
-
-	public List<GrantedAuthority> getAuthorities(String token) {
-		Claims claims = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
-				.build().parseClaimsJws(token).getBody();
-		String role = claims.get("roles", String.class);
-		if (role != null) {
-			return List.of(new SimpleGrantedAuthority("ROLE_" + role));
-		}
-		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-	}
+//	public boolean validateToken(String token) {
+//		try {
+//			Claims claims = Jwts.parserBuilder()
+//					.setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8))).build()
+//					.parseClaimsJws(token).getBody();
+//
+//			return claims.getExpiration() != null && claims.getExpiration().after(new Date());
+//		} catch (Exception e) {
+//			return false;
+//		}
+//	}
+//
+//	public String extractUsername(String token) {
+//		return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8))).build()
+//				.parseClaimsJws(token).getBody().getSubject();
+//	}
+//
+//	public List<GrantedAuthority> getAuthorities(String token) {
+//		Claims claims = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
+//				.build().parseClaimsJws(token).getBody();
+//		String role = claims.get("roles", String.class);
+//		if (role != null) {
+//			return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+//		}
+//		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+//	}
 
 }
