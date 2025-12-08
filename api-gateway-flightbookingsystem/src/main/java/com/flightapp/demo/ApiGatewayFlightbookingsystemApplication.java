77bf9537d5b10c29bc176f4f6a75be2360095791
Pranitha <pranithareddy160805@gmail.com>
2025-12-08 20:57:28 +0530

    Added api gateway

---
+++ api-gateway-flightbookingsystem/src/main/java/com/flightapp/demo/ApiGatewayFlightbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.flightapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayFlightbookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayFlightbookingsystemApplication.class, args);
+	}
+
+}
